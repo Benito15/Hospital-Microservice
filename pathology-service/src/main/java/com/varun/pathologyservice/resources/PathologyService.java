462259f3416636f15a568561081ccff4ad4fdc76
varunu28 <varun.u28@gmail.com>
2019-07-21 20:11:54 -0700

    Added code for Pathology service

---
+++ pathology-service/src/main/java/com/varun/pathologyservice/resources/PathologyService.java
@@ -0,0 +1,32 @@
+package com.varun.pathologyservice.resources;
+
+import com.varun.pathologyservice.model.Disease;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/pathology")
+public class PathologyService {
+    List<Disease> diseases = Arrays.asList(
+            new Disease("D1", "Asthma", "Clean air"),
+            new Disease("D2", "Common Cold", "Chicken Soup"),
+            new Disease("D3", "Headache", "Green Tea")
+    );
+
+    @RequestMapping("/diseases")
+    private List<Disease> getDiseases() {
+        return diseases;
+    }
+
+    @RequestMapping("/diseases/{id}")
+    private Disease getDiseaseById(@PathVariable("id") String id) {
+        return diseases.stream()
+                .filter(disease -> disease.getId().equals(id))
+                .findAny()
+                .orElse(null);
+    }
+}
